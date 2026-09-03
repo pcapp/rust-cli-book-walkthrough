95f2d725520075be1281d1ca317adad700f8d9bb
Peter Cappetto <peter.cappetto@gmail.com>
2026-09-03 10:20:21 -0700

    Echo the text and optionally omit the newline.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
+use clap::{Arg, ArgAction, Command};
+
 fn main() {
-    for (index, arg) in std::env::args().enumerate() {
-        println!("[{index}] {arg}")
-    }
+    let matches = Command::new("echor")
+        .author("peter.cappetto@gmail.com")
+        .arg(
+            Arg::new("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .num_args(1..)
+        )
+        .arg(
+            Arg::new("omit_newline")
+                .short('n')
+                .action(ArgAction::SetTrue)
+                .help("Do not print a newline")
+        )
+        .get_matches();
+
+    //let text: Vec<&String> = matches.get_many("text").unwrap().collect();
+    let text: Vec<String> = matches.get_many("text").unwrap().cloned().collect();
+
+    let ending = if matches.get_flag("omit_newline") {""} else {"\n"};
+
+    print!("{}{ending}", text.join(" "));
 }
