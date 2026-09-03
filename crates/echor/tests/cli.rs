2512b721997c3f3be8c20b07b9aa0982ad13409d
Peter Cappetto <peter.cappetto@gmail.com>
2026-09-03 12:57:15 -0700

    Factor out common testing code

--- crates/echor/tests/cli.rs
+++ crates/echor/tests/cli.rs
@@ -26,18 +26,30 @@ fn stock_dies_no_args() -> Result<()> {
 
 #[test]
 fn hello1() -> Result<()> {
-    let outfile = "tests/expected/hello1.txt";
-    let expected = fs::read_to_string(outfile)?;
-    let mut cmd = Command::cargo_bin("echor")?;
-    cmd.arg("Hello there").assert().success().stdout(expected);
-    Ok(())
+    run(&["Hello", "there"], "tests/expected/hello2.txt")
+}
+
+#[test]
+fn hello1_no_newline() -> Result<()> {
+    run(&["-n", "Hello there"], "tests/expected/hello1.n.txt")
 }
 
 #[test]
 fn hello2() -> Result<()> {
-    let outfile = "tests/expected/hello2.txt";
-    let expected = fs::read_to_string(outfile)?;
+    run(&["Hello there"], "tests/expected/hello1.txt")
+}
+
+#[test]
+fn hello2_no_newline() -> Result<()> {
+    run(&["-n", "Hello", "there"], "tests/expected/hello2.n.txt")
+}
+
+fn run(args: &[&str], expected_file: &str) -> Result<()> {
+    let expected = fs::read_to_string(expected_file)?;
     let mut cmd = Command::cargo_bin("echor")?;
-    cmd.args(vec!["Hello", "there"]).assert().success().stdout(expected);
+    let output = cmd.args(args).output()?;
+
+    let stdout = String::from_utf8(output.stdout).expect("invalid UTF-8");
+    assert_eq!(stdout, expected);
     Ok(())
 }
