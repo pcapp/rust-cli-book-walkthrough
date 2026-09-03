9823a26c124706224b6c655db2cdc33d0aa5e213
Peter Cappetto <peter.cappetto@gmail.com>
2026-09-03 12:43:34 -0700

    Test multiple words in the text.

--- crates/echor/tests/cli.rs
+++ crates/echor/tests/cli.rs
@@ -1,10 +1,12 @@
 use std::fs;
 use assert_cmd::Command;
+use anyhow::Result;
 
 #[test]
-fn dies_no_args() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn dies_no_args() -> Result<()> {
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.assert().failure().stderr(predicates::str::contains("Usage"));
+    Ok(())
 }
 
 /*
@@ -13,18 +15,29 @@ fn dies_no_args() {
     by using external crates.
  */
 #[test]
-fn stock_dies_no_args() {
+fn stock_dies_no_args() -> Result<()> {
     use std::process::Command;
 
-    let output = Command::new("../../target/debug/echor").output().unwrap();
+    let output = Command::new("../../target/debug/echor").output()?;
     // Status code 2 is an argument error
     assert_eq!(output.status.code().unwrap(), 2);
+    Ok(())
 }
 
 #[test]
-fn hello1() {
+fn hello1() -> Result<()> {
     let outfile = "tests/expected/hello1.txt";
-    let expected = fs::read_to_string(outfile).unwrap();
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+    let expected = fs::read_to_string(outfile)?;
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.arg("Hello there").assert().success().stdout(expected);
+    Ok(())
+}
+
+#[test]
+fn hello2() -> Result<()> {
+    let outfile = "tests/expected/hello2.txt";
+    let expected = fs::read_to_string(outfile)?;
+    let mut cmd = Command::cargo_bin("echor")?;
+    cmd.args(vec!["Hello", "there"]).assert().success().stdout(expected);
+    Ok(())
 }
